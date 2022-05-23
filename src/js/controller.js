bb8931e937e185860f6ae9e893d57b2997601b59
Kevin Kularathne <kevin.d.kularathna@gmail.com>
2022-05-23 18:54:05 +0530

    added loading icons

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,6 +7,7 @@ const binaryImageSpace = document.querySelector(".binary-image-space");
 const detectedImageSpace = document.querySelector(".detected-image-space");
 const binaryImage = document.querySelector(".binary-image");
 const detectedImage = document.querySelector(".detected-image");
+const loadingIcon = document.querySelector(".loading-icon");
 
 if (formParas) {
   formParas.addEventListener("submit", function (e) {
@@ -16,6 +17,7 @@ if (formParas) {
     FR.readAsDataURL(photo.files[0]);
 
     FR.addEventListener("load", async function (e) {
+      loadingIcon.classList.remove("hidden");
       const res = await fetch("http://127.0.0.1:5000/test_parameters", {
         method: "POST",
         body: JSON.stringify({
@@ -25,7 +27,8 @@ if (formParas) {
         }),
       });
       const data = await res.json();
-      
+
+      loadingIcon.classList.add("hidden");
       binaryImageSpace.classList.remove("hidden");
       detectedImageSpace.classList.remove("hidden");
       binaryImage.src = data.binary_img;
